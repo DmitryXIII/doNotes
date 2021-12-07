08bef4b21f2a3c4e63d276d99c46333dc236591c
Dmitry Gusev <dmtrxiii@gmail.com>
2021-12-07 14:55:59 +0300

    navigation drawer menu

--- app/src/main/java/com/ineedyourcode/donotes/ui/MainActivity.java
+++ app/src/main/java/com/ineedyourcode/donotes/ui/MainActivity.java
@@ -2,19 +2,27 @@ package com.ineedyourcode.donotes.ui;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentResultListener;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationView;
 import com.ineedyourcode.donotes.R;
 import com.ineedyourcode.donotes.domain.Note;
+import com.ineedyourcode.donotes.navdrawer.AboutAppFragment;
+import com.ineedyourcode.donotes.navdrawer.SettingsFragment;
 import com.ineedyourcode.donotes.ui.list.NotesListFragment;
 import com.ineedyourcode.donotes.ui.notecontent.NoteContentFragment;
 
 public class MainActivity extends AppCompatActivity {
     private static String ARG_NOTE = "ARG_NOTE";
     private static Note selectedNote;
+    private DrawerLayout navDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +61,32 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
+
+        navDrawer = findViewById(R.id.nav_drawer);
+
+        NavigationView navigationView = findViewById(R.id.menu_nav_drawer);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_about:
+                        menuAction(new AboutAppFragment(), "aboutAppFragment");
+                    case R.id.action_settings:
+                        menuAction(new SettingsFragment(), "settingsFragment");
+                }
+                return false;
+            }
+        });
+    }
+
+    private boolean menuAction(Fragment fragment, String tag) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment, tag)
+                .addToBackStack(tag)
+                .commit();
+        navDrawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
